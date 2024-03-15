289fb398ceb2be86d054320f319592b344b4e567
Ilias ASSADKI <68815850+iassadki@users.noreply.github.com>
2024-03-15 15:17:40 +0100

    Update webtoapp_options.php
    Making all the substrings of the file translatable.

--- webtoapp_options.php
+++ webtoapp_options.php
@@ -90,21 +90,21 @@ class WtadOptions
     {
         $context = $responseOK? "success" : "danger";
         
-        $resp = $responseText === null? "" : "<div class='alert alert-" . esc_attr($context). "' role='alert'>" . esc_html($responseText) . "</div>";
+        $resp = $responseText === null ? "" : "<div class='alert alert-" . esc_attr($context) . "' role='alert'>" . esc_html($responseText) . "</div>";
         
         $logo =  esc_url( plugins_url('back/logo.svg', __FILE__) );
         
         $out = "";
-        $out .= "<nav class='navbar navbar-expand-md navbar-light bg-light fixed-top shadow-sm' id='navigation-bar'>";
-        $out .= "<a class='navbar-brand' href='https://webtoapp.design/?utm_source=wordpress&utm_medium=plugin&utm_campaign=header'>";
+        $out .= '<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top shadow-sm" id="navigation-bar">';
+        $out .= '<a class="navbar-brand" href="https://webtoapp.design/?utm_source=wordpress&utm_medium=plugin&utm_campaign=header">';
         $out .= "<img decoding='async' src='{$logo}' style='height:2rem; width: auto' height='144' width='144' class='d-inline-block align-top' alt='Website to App Converter webtoapp.design Logo'>";
-        $out .= "webtoapp.design";
-        $out .= "</a>";
+        $out .= "<span style='float: right; margin-left:15px; margin-top:5px; font-size: 20px;'> webtoapp.design </span>";
+        $out .= '</a>';
 
-        $out .= "<div class='collapse navbar-collapse' id='navbarResponsive'>";
+        $out .= '<div class="collapse navbar-collapse" id="navbarResponsive">';
 
-        $out .= "</div>";
-        $out .= "</nav>";
+        $out .= '</div>';
+        $out .= '</nav>';
 
         $out .=  wp_kses_post($resp);
         
@@ -116,101 +116,100 @@ class WtadOptions
         $turnOn  = $on? "btn btn-primary active" : "btn btn-secondary";
         $turnOff = $on? "btn btn-secondary" : "btn btn-primary active";
         
-        $text = $on? "An automatic notification will be sent when a new page is made public. The notification will contain the post's title and, if available, it's featured image."
-                    :"No automatic notification will be sent when a new page is made public."; 
-        
+        $text = $on ? __('An automatic notification will be sent when a new page is made public. The notification will contain the post\'s title and, if available, it\'s featured image.', 'webtoapp-design')
+                    : __('No automatic notification will be sent when a new page is made public.', 'webtoapp-design');
+
         $text = esc_html($text);
                     
-        $last = $lastAutoResponse == null? "" : "Last notification: " . esc_html($lastAutoResponse);
+        $last = $lastAutoResponse == null? "" : __('Last notification:', 'webtoapp-design') . " " . esc_html($lastAutoResponse);
                            
-        $out = "";
-        $out .= "<div class='card mt-3'>";
-        $out .= "  <div class='card-header'>";
-        $out .= "<h3 class='title'>New Page Publish Notification</h3>";
-        $out .= "</div>";
-        $out .= "<div class='card-body'>";
-        $out .= "<form action='' method='post' enctype='multipart/form-data'>";
+        $out = '';
+        $out .= '<div class="card mt-3">';
+        $out .= '  <div class="card-header">';
+        $out .= '<h3 class="title">'  . esc_html__('New Page Publish Notification', 'webtoapp-design') . '</h3>';
+        $out .= '</div>';
+        $out .= '<div class="card-body">';
+        $out .= '<form action="" method="post" enctype="multipart/form-data">';
         $out .= wp_nonce_field('wtad', '_wpnonce', true, false);
-        $out .= "<div class='btn-group btn-group-toggle' data-toggle='buttons' >";
-        $out .= "<input style='color:white' class='" . esc_attr($turnOn) . "'  type='submit' name='webapp-auto-on'  autocomplete='off' value='On' />";
-        $out .= "<input style='color:white' class='" . esc_attr($turnOff)."' type='submit' name='webapp-auto-off' autocomplete='off' value='Off'/>";
-        $out .= "</div>";
-        $out .= "<br/><br/>";
-        $out .= "<p>" . wp_kses_post($text) . "</p>";
-        $out .= "<p>" . wp_kses_post($last) . "</p>";
-        $out .= "</form>";
-        $out .= "</div>";
-        $out .= "</div>";
+        $out .= '<div class="btn-group btn-group-toggle" data-toggle="buttons" >';
+        $out .= '<input style="color:white" class="' . esc_attr($turnOn) . '"  type="submit" name="webapp-auto-on"  autocomplete="off" value="' . esc_attr__('On', 'webtoapp-design') . '"/>';
+        $out .= '<input style="color:white" class="' . esc_attr($turnOff). '" type="submit" name="webapp-auto-off" autocomplete="off" value="' . esc_attr__('Off', 'webtoapp-design') . '"/>';
+        $out .= '</div>';
+        $out .= '<br/><br/>';
+        $out .= '<p>' . wp_kses_post($text) . '</p>';
+        $out .= '<p>' . wp_kses_post($last) . '</p>';
+        $out .= '</form>';
+        $out .= '</div>';
+        $out .= '</div>';
         
         return $out;
     }
     
     function push()
     {
-        $dd = WtadMedia::dropdownPages("dd_url", "Select a page", "url_to_open");
-        
-        $out = "";
-        $out .= "<div class='card mt-3'>";
-        $out .= "  <div class='card-header'>";
-        $out .= "<h3 class='title'>Push Notifications</h3>";
-        $out .= "</div>";
-        $out .= "<div class='card-body'>";
-        $out .= "<form action='' method='post' enctype='multipart/form-data'>";
+        $dd = WtadMedia::dropdownPages("dd_url", __('Select a page', 'webtoapp-design'), "url_to_open");        
+
+        $out = '';
+        $out .= '<div class="card mt-3">';
+        $out .= '  <div class="card-header">';
+        $out .= '<h3 class="title">' . esc_html__('Push Notifications', 'webtoapp-design') . '</h3>'; 
+        $out .= '</div>';
+        $out .= '<div class="card-body">';
+        $out .= '<form action="" method="post" enctype="multipart/form-data">';
         $out .= wp_nonce_field('wtad', '_wpnonce', true, false);
-        $out .= "<p>The title or main message of your push notification.</p>";
-        $out .= "<div class='form-group'>";
-        $out .= "<div class='input-group'>";
-        $out .= "<input name='title' class='form-control' placeholder='Title' id='title' required='' type='text' >";
-        $out .= "<label for='title' class='sr-only'>Title</label>";
-        $out .= "<p class='text-danger' id='regex-error-title' hidden=''></p>";
-        $out .= "</div>";
-        $out .= "</div>";
-        $out .= "<p>An optional, longer message that is shown below the title.</p>";
-        $out .= "<div class='form-group'>";
-        $out .= "<div class='input-group'>";
-        $out .= "<input name='message' class='form-control' placeholder='Message (optional)' id='message' type='text' >";
-        $out .= "<label for='message' class='sr-only'>Message (optional)</label>";
-        $out .= "<p class='text-danger' id='regex-error-message' hidden=''></p>";
-        $out .= "</div>";
-        $out .= "</div>";
-        $out .= "<p> This link will be opened inside your app when the notification is clicked. <a href='https://webtoapp.design/blog/send-push-notification#tracking-notification-clicks'> Here's how you can track how many users are opening your notifications. </a>";
-        $out .= "</p>";
-        $out .= "<div class='form-group'>";
-        $out .= "<div class='input-group'>";
-        $out .= "<input name='url_to_open' class='form-control' placeholder='Link to Open on Notification Click (optional)' id='url_to_open' type='url' >";
-        $out .= "<label for='url_to_open' class='sr-only'>Link to Open on Notification Click (optional)</label>";
+        $out .= '<p>' . esc_html__('The title or main message of your push notification', 'webtoapp-design') . '</p>';
+        $out .= '<div class="form-group">';
+        $out .= '<div class="input-group">';
+        $out .= '<input name="title" class="form-control" placeholder="' . esc_attr__('Title', 'webtoapp-design') . '" id="title" required="" type="text" >';
+        $out .= '<label for="title" class="sr-only">'  . esc_html__('Title', 'webtoapp-design') . '</label>';
+        $out .= '<p class="text-danger" id="regex-error-title" hidden=""></p>';
+        $out .= '</div>';
+        $out .= '</div>';
+        $out .= '<p>'  . esc_html__('An optional, longer message that is shown below the title.', 'webtoapp-design') . '</p>';
+        $out .= '<div class="form-group">';
+        $out .= '<div class="input-group">';
+        $out .= '<input name="message" class="form-control" placeholder="' . esc_attr__('Message (optional)', 'webtoapp-design') . '" id="message" type="text" >';
+        $out .= '<label for="message" class="sr-only">' . esc_html__('Message (optional)', 'webtoapp-design') . '</label>';
+        $out .= '<p class="text-danger" id="regex-error-message" hidden=""></p>';
+        $out .= '</div>';
+        $out .= '</div>';
+        $out .= '<p> ' . esc_html__('This link will be opened inside your app when the notification is clicked.', 'webtoapp-design') . ' <a href="https://webtoapp.design/blog/send-push-notification#tracking-notification-clicks"> ' . esc_html__('Here\'s how you can track how many users are opening your notifications.', 'webtoapp-design') . '</a>';
+        $out .= '</p>';
+        $out .= '<div class="form-group">';
+        $out .= '<div class="input-group">';
+        $out .= '<input name="url_to_open" class="form-control" placeholder="' . esc_attr__('Link to Open on Notification Click (optional)', 'webtoapp-design') . '" id="url_to_open" type="url" >';
+        $out .= '<label for="url_to_open" class="sr-only">' . esc_html__('Link to Open on Notification Click (optional)', 'webtoapp-design') . '</label>';
 
-        $out .= "<p class='text-danger' id='regex-error-url_to_open' hidden=''></p>";
-            
-
-        $out .= "<div class='input-group-append'>{$dd}</div>";
+        $out .= '<p class="text-danger" id="regex-error-url_to_open" hidden=""></p>';
+                    
+        $out .= "<div class='input-group-append'>{$dd}</div>"; 
          
-        $out .= "</div>";
+        $out .= '</div>';
 
-        $out .= "</div>";
-        $out .= "<p>A link to an image that will be attached to your push notification. Requirements:</p>";
-        $out .= "<ul>";
-        $out .= "<li>Image in PNG or JPG format</li>";
-        $out .= "<li>Image size smaller than 300KB</li>";
+        $out .= '</div>';
+        $out .= '<p>'  . esc_html__('A link to an image that will be attached to your push notification. Requirements:', 'webtoapp-design') . '</p>';
+        $out .= '<ul>';
+        $out .= '<li>'  . esc_html__('Image in PNG or JPG format', 'webtoapp-design') . '</li>';
+        $out .= '<li>'  . esc_html__('Image size smaller than 300KB', 'webtoapp-design') . '</li>';
         
-        $out .= "</ul>";
-        $out .= "<div class='form-group'>";
-        $out .= "<div class='input-group'>";
-        $out .= "<input name='image_url' class='form-control' placeholder='Image Link (optional)' id='image_url' type='url' >";
-        $out .= "<label for='image_url' class='sr-only'>Image Link (optional)</label>";
-        $out .= "<p class='text-danger' id='regex-error-image_url' hidden=''></p>";
-        $out .= "<div class='input-group-append'>";
-        $out .= "<button class='btn btn-primary' type='button' id='image_media_gallery' >Media Gallery</button>";
-        $out .= "</div>";
-        $out .= "</div>";
-        $out .= "</div>";
-        $out .= "<input id='csrf_token' name='csrf_token' type='hidden' value='IjFjZDEyYmFkMmI0YjJmMzNmZWMwYWNkZGFiNTcwMTkyYjI1MzlkOWIi.ZP9wDw.KRs8sJse6_nAyrl0rAlrT0Smvf8'>";
-        $out .= "<button type='submit' class='btn btn-primary mt-2 btn-block' name='send_notification'>Send Notification</button>";
-        $out .= "</form>";
-        $out .= "<p class='mt-3'> Having difficulties? <a href='https://webtoapp.design/blog/send-push-notification'>Here's our guide to sending push notifications</a> including a section about <a href='https://webtoapp.design/blog/send-push-notification#not-receiving-notifications'>why users might not be receiving notifications.</a>";
-        $out .= "</p>";
-        $out .= "</div>";
-        $out .= "</div>";
+        $out .= '</ul>';
+        $out .= '<div class="form-group">';
+        $out .= '<div class="input-group">';
+        $out .= '<input name="image_url" class="form-control" placeholder="'  . esc_attr__('Image Link (optional)', 'webtoapp-design') . '" id="image_url" type="url" >';
+        $out .= '<label for="image_url" class="sr-only">'  . esc_html__('Image Link (optional)', 'webtoapp-design') . '</label>';
+        $out .= '<p class="text-danger" id="regex-error-image_url" hidden=""></p>';
+        $out .= '<div class="input-group-append">';
+        $out .= '<button class="btn btn-primary" type="button" id="image_media_gallery" >'  . esc_html__('Media Gallery', 'webtoapp-design') . '</button>';
+        $out .= '</div>';
+        $out .= '</div>';
+        $out .= '</div>';
+        $out .= '<input id="csrf_token" name="csrf_token" type="hidden" value="IjFjZDEyYmFkMmI0YjJmMzNmZWMwYWNkZGFiNTcwMTkyYjI1MzlkOWIi.ZP9wDw.KRs8sJse6_nAyrl0rAlrT0Smvf8">';
+        $out .= '<button type="submit" class="btn btn-primary mt-2 btn-block" name="send_notification">'  . esc_html__('Send Notification', 'webtoapp-design') . '</button>';
+        $out .= '</form>';
+        $out .= '<p class="mt-3">'  . esc_html__('Having difficulties?', 'webtoapp-design') . ' <a href="https://webtoapp.design/blog/send-push-notification"> '  . esc_html__('Here\'s our guide to sending push notifications', 'webtoapp-design') . '</a> '  . esc_html__('including a section about', 'webtoapp-design') . ' <a href="https://webtoapp.design/blog/send-push-notification#not-receiving-notifications"> '  . esc_html__('why users might not be receiving notifications.', 'webtoapp-design') . '</a>';
+        $out .= '</p>';
+        $out .= '</div>';
+        $out .= '</div>';
 
         $out .= $this->media->getNecessaryJs("#image_url", "#image_media_gallery");
         return $out;
@@ -230,7 +229,7 @@ class WtadOptions
         
         echo wp_kses($content, array(
             "button" => $permit,
-            "div"    => $permit,
+            "div"    => $permit, 
             "ul"     => $permit,
             "li"     => $permit,
             "form"   => $permit,
@@ -311,21 +310,25 @@ class WtadOptions
             <form id="webtoapp-set-key" method="post" enctype="multipart/form-data" action="" class="webtoapp-dashboard" >  
             	
             	<?php wp_nonce_field('wtad') ?>
+
+                <!-- <?php esc_html_e('', 'webtoapp-design'); ?> -->
+                <!-- value="<?php echo esc_attr__('', 'webtoapp-design'); ?>" -->
             	
-    			<h1>Enter your API key:</h1>
+                <h1><?php esc_html_e('Enter your API key:', 'webtoapp-design'); ?></h1>
 
     			<div class="input-group">
                     <input name="webtoapp-key" class="form-control" placeholder="12345-abcdefghijklmnopqrstuvwxyz-0123456789" id="webtoapp-key" type="text" minlength="43" maxlength="43">
-                    <label for="webtoapp-key" class="sr-only">Your API key</label>
+                    <label for="webtoapp-key" class="sr-only"><?php esc_html_e('Your API key', 'webtoapp-design'); ?></label>
                     <div class="input-group-append">
-                        <button class="btn btn-primary" type="submit" >Save</button>
+                        <button class="btn btn-primary" type="submit" ><?php esc_html_e('Save', 'webtoapp-design'); ?></button>
                     </div>
                 </div>
 
-    			<br/><a href="https://webtoapp.design/?utm_source=wordpress&utm_medium=plugin&utm_campaign=login" target="_blank" >I have not created an app yet</a>
+    			<br/><a href="https://webtoapp.design/?utm_source=wordpress&utm_medium=plugin&utm_campaign=login" target="_blank" ><?php esc_html_e('I have not created an app yet', 'webtoapp-design'); ?></a>
     			&nbsp;|&nbsp;
     			
-    			<a href="https://webtoapp.design/redirector/app_id/dashboard_bp.developer_tools?utm_source=wordpress&utm_medium=plugin&utm_campaign=login" target="_blank" >Find my app's API key</a>
+    			<a href="https://webtoapp.design/redirector/app_id/dashboard_bp.developer_tools?utm_source=wordpress&utm_medium=plugin&utm_campaign=login" target="_blank" ><?php esc_html_e('Find my app\'s API key', 'webtoapp-design'); ?></a>
+
     			
     		</form>
     				    			
@@ -340,7 +343,7 @@ class WtadOptions
            	    <?php wp_nonce_field('wtad') ?>
     			<br/>
     			<p>
-    			<input class="btn btn-danger url-submit-button" type="submit" value="Delete API Key" name="webtoapp-key-delete" />
+    			<input class="btn btn-danger url-submit-button" type="submit" value="<?php echo esc_attr_e('Delete API Key', 'webtoapp-design'); ?>" name="webtoapp-key-delete" />
     			<?php echo esc_html("&nbsp;(" . $options["key"] .")")  ?>
     			</p>
            </form>
